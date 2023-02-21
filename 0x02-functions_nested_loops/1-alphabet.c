fda29c02bd3b7271d417203f74bf7f051518c046
Nzivohelsie <kavulinzivo@gmail.com>
2023-02-21 12:35:23 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_alphabet - print lowercase alphabet
+ *
+ *Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+}
