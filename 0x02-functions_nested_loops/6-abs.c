18d9129e5512c145252441a8f2e26858ad1ce5cf
Nzivohelsie <kavulinzivo@gmail.com>
2023-02-22 05:06:25 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - print absolute number
+ * @a: parmeter 1
+ * Return: always 0
+ */
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		n = n;
+	}
+	else
+	{
+		n = -(n);
+	}
+	return (n);
+}
