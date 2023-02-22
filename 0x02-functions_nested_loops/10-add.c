f0a7d9c4c3a52340fa6de0bb1244b5f12dabd128
Nzivohelsie <kavulinzivo@gmail.com>
2023-02-22 09:52:00 -0800

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - sum
+ * @a: interger
+ * @b: interger
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - times table
+ */
+void times_table(void)
+{
+	int m;
+	int n;
+	int t;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			t = n * m;
+			if (t
+			printf("%d,  ", t);
+		}
+		printf("\n");
+	}
+}
