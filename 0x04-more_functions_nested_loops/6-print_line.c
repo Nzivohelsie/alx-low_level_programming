3eb5fd2962df5a54085d80bae09977483ea199ec
Nzivohelsie <kavulinzivo@gmail.com>
2023-02-23 16:29:06 -0800

    6-lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - print dashes n times
+ * @n: parameter
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; ++i)
+		{
+			putchar('_');
+		}
+		putchar('\n');
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
