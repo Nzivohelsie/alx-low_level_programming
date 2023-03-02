ab720ce898422b32812cd3df7aa7e278b45e9249
Nzivohelsie <kavulinzivo@gmail.com>
2023-03-02 03:08:11 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - rotate letters by 13
+ * @n: parameter
+ * Return: n
+ */
+char *rot13(char *n)
+{
+	char a [] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b [] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, z;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (z = 0; a[z] != '\0'; z++)
+		{
+			if (n[i] == a[z])
+			{
+				n[i] = b[z];
+				break;
+			}
+		}
+	}
+	return (n);
+}
