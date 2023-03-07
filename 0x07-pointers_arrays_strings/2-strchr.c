79b36a250fb4cceefa642ddd8db21a5e46f3b154
Nzivohelsie <kavulinzivo@gmail.com>
2023-03-07 13:45:57 -0800

    find character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - find character
+ *@s: string
+ *@c: character to find
+ *Return: always 0 success
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
