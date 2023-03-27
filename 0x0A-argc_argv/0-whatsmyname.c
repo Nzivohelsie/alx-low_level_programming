855e0e52ed4bdebb37e500beb1d5d15a00631a39
Nzivohelsie <kavulinzivo@gmail.com>
2023-03-27 11:46:19 -0700

    betty doc style fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: array of pointer to input strings
  * Return: 0 success
  */
-int main(int argc, char *argv [])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
