c55064e4be084fe6dd37dfb29839f66b75804bfb
Nzivohelsie <kavulinzivo@gmail.com>
2023-04-21 12:19:02 -0700

    ensured chars_write = chars_read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,30 +6,33 @@
 #include <stdlib.h>
 /**
  * read_textfile - read and print to posix std output
- * file name: pints to file to be read
- * letters: no. of bytes to be read
+ * @filename: pints to file to be read
+ * @letters: no. of bytes to be read
  *
  * Return: no of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t chars_read, chars_write;
+	ssize_t chars_read, chars_write, j;
 	void *buffer;
+	size_t i = 0;
 
 	buffer = malloc(letters * sizeof(char));
-	if(filename == NULL)
+	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDWR);
-	if(fd == -1)
+	if (fd == -1)
 		return (0);
 	chars_read = read(fd, buffer, letters);
 	if (chars_read == -1)
-		return(0);
-	chars_write = write(STDOUT_FILENO, buffer, letters);
-	if(chars_write == -1)
+		return (0);
+	for (j = 0; j < chars_read; j++)
+		i += 1;
+	chars_write = write(STDOUT_FILENO, buffer, i);
+	if (chars_write == -1 || chars_write != chars_read)
 		return (0);
 	free(buffer);
 	close(fd);
-	return(chars_read);
+	return (chars_read);
 }
