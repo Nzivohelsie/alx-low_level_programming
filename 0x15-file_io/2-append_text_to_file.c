4f4a1591881316047207eaea5f7275e81d707ceb
Nzivohelsie <kavulinzivo@gmail.com>
2023-04-22 00:09:15 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,23 +15,24 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, j;
 	ssize_t chars_write;
 	ssize_t chars_read = 0;
-	size_t i;
+	size_t i = 0;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-		if (fd == -1)
-			return (-1);
-	for (j = 0; text_content[j] != '\0'; j++)
-	{
-		i += 1;
-		chars_read += 1;
-	}
+	if (fd == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
+		for (j = 0; text_content[j] != '\0'; j++)
+		{
+			i += 1;
+			chars_read += 1;
+		}
 		chars_write = write(fd, text_content, i);
 		if (chars_write == -1 || chars_write != chars_read)
 			return (-1);
 	}
+	close(fd);
 	return (1);
 }
